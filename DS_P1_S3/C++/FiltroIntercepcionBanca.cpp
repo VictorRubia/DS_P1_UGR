0a025f0434e47668796e77d2cc8cf855ed5ca53e
VictorRubia <victorrubia@correo.ugr.es>
2021-03-29 18:20:56 +0200

    empezamos

--- DS_P1_S3/C++/FiltroIntercepcionBanca.cpp
+++ DS_P1_S3/C++/FiltroIntercepcionBanca.cpp
@@ -8,16 +8,13 @@
 using namespace std;
 
 int main(){
-    cout<<"prueba";
-    // El cliente quiere pedir un Préstamo de 15000 a 10 meses con un interés del 1,2
+
     Cliente* miCliente = new Cliente();
     Filtro *rentabilidad = new CompruebaRentabilidad();
 
     miCliente->getGestorFiltros()->setFiltros(rentabilidad);
 
     miCliente->pedirPrestamo(miCliente->rellenarFormulario());
-
-    cout << "Prueba para Claudia" << endl;
 
     cout << "Finalmente el préstamo es de" << miCliente->getMiPrestamo().getImporte() << " € a pagar en " << miCliente->getMiPrestamo().getNum_cuotas() << " meses. Interés del " << miCliente->getMiPrestamo().getInteres() << endl;
 
