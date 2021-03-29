7f0dd6af4df89f6af70272f3757973117e0baeaa
VictorRubia <victorrubia@correo.ugr.es>
2021-03-29 18:13:07 +0200

    He hecho tal y cual

--- DS_P1_S3/C++/FiltroIntercepcionBanca.cpp
+++ DS_P1_S3/C++/FiltroIntercepcionBanca.cpp
@@ -17,6 +17,8 @@ int main(){
 
     miCliente->pedirPrestamo(miCliente->rellenarFormulario());
 
+    cout << "Prueba para Claudia" << endl;
+
     cout << "Finalmente el préstamo es de" << miCliente->getMiPrestamo().getImporte() << " € a pagar en " << miCliente->getMiPrestamo().getNum_cuotas() << " meses. Interés del " << miCliente->getMiPrestamo().getInteres() << endl;
 
     return 0;
